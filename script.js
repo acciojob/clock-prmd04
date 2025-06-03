703cab4d61a6bee50a1d2a2c8009ea01616e56ef
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-03 18:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let time = document.getElementById("timer");
+
+let currentTime = new Date();
+
+setInterval(function(){
+	currentTime = new Date();
+
+	time.innerHTML = currentTime.toLocaleString();
+},1000)
